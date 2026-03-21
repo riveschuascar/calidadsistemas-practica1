90ac90cfde254cbd752a278a3f7aa811f930ae64
ciriarte-dev <carlos.iriarte.dev@gmail.com>
2026-03-20 22:12:58 -0400

    refactor: remove unused imports form tarjetas-usuarios controller

--- backend/src/endpoints/tarjetas-usuarios/tarjetas-usuarios.controller.ts
+++ backend/src/endpoints/tarjetas-usuarios/tarjetas-usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { TarjetasUsuarios } from './tarjetas-usuarios.entity';
 import { TarjetasUsuariosService } from './tarjetas-usuarios.service';
 
