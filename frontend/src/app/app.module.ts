f1a7217cb83d00f420ccb0f153a5456dc4ddcbbd
unknown <huascar.rivero@ucb.edu.bo>
2026-03-22 16:25:33 -0400

    refact: app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';  // Import FormsModule
 import { routes } from './app.routes';
